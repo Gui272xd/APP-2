45f9fafd1568de8cdb15df9c963f79386dd438e9
Gui272xd <lopesguilherme272xd@gmail.com>
2025-11-19 11:34:28 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
-import 'package:aula_6/screens/myhomepage.dart';
+import 'package:desafio1/pages/MyHomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Aula-6',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
+  runApp(const Myhomepage());
 }
