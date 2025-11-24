e0058d75781490d937394b06beb056c06a50c208
Gui272xd <lopesguilherme272xd@gmail.com>
2025-11-24 11:40:33 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:desafio1/pages/MyHomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:form_flutter/myapp.dart';
 
 void main() {
-  runApp(const Myhomepage());
+  runApp(MyApp());
 }
